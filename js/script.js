63f515dd337b8f6993c82cfde2aa8deb50e09c52
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-05-22 01:55:22 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function reverseString() {
+  var input = document.getElementById("string").value;
+  var reversed = "";
+  for (var counter = input.length - 1; counter >= 0; counter--) {
+    reversed += input.charAt(counter);
+  }
+  document.getElementById("output").textContent = reversed;
+}
