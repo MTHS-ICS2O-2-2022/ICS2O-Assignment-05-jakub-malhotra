32d5aca0b6faba6adef6d69c5e8534014decb72e
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-05-23 00:02:37 +0000

    go extra

---
+++ go_app/main.go
@@ -0,0 +1,32 @@
+// Created by: Jakub Malhotra
+// Created on: May 2023
+//
+// This program calculates the volume of a pyramid
+
+package main
+
+import (
+	"fmt"
+)
+
+func calculate() {
+	var input int
+	fmt.Print("Enter a number: ")
+	fmt.Scanln(&input)
+
+	number := input
+
+	digitCount := 0
+	for number != 0 {
+		number = number / 10
+		digitCount++
+	}
+
+	fmt.Println("Total number of digits:", digitCount)
+}
+
+func main() {
+	calculate()
+
+	fmt.Println("\nDone.")
+}
